0fad4b93f04e5960b880a7f35e0b4fe81db2e95e
thymac <thywillberdone@gmail.com>
2023-08-01 22:10:16 +0000

    revise

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,20 +1,28 @@
-// Export a constant instance of WeakMap and name it weakMap
+/**
+ * A weak map of endpoints and the number of calls made.
+ */
 export const weakMap = new WeakMap();
 
-// Export a new function named queryAPI
-export function queryAPI(endpoint) {
-  // If the endpoint is not in the weakMap, initialize it with a count of 1
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
-  } else {
-    // If the endpoint is already in the weakMap, increment the count
-    const count = weakMap.get(endpoint) + 1;
-    weakMap.set(endpoint, count);
+/**
+ * The maximum number of calls for an endpoint.
+ */
+const MAX_ENDPOINT_CALLS = 5;
 
-    // Check if the count is >= 5, and if so, throw an error
-    if (count >= 5) {
-      throw new Error('Endpoint load is high');
-    }
+/**
+ * Tracks the number of calls made to an API's endpoint.
+ * @param {{
+ *   protocol: String,
+ *   name: String,
+ * }} endpoint - The endpoint to make a request to.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+    throw new Error('Endpoint load is high');
   }
 }
 
