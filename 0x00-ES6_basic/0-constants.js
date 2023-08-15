77c8212c760799616d528e7f50f5eda7949e4bda
thymac <thywillberdone@gmail.com>
2023-08-15 09:43:54 +0000

    revised

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,17 +1,15 @@
 export function taskFirst() {
-  const task = 'I prefer const when I can.'; // Declare a constant variable
+  const task = 'I prefer const when I can.';
   return task;
 }
 
-// Returns a string indicating that something is okay
 export function getLast() {
   return ' is okay';
 }
 
-// Combines the result of getLast() with a message and returns the combination
 export function taskNext() {
-  let combination = 'But sometimes let'; // Declare a variable using let
-  combination += getLast(); // Append the result of getLast() to the variable
+  let combination = 'But sometimes let';
+  combination += getLast();
 
   return combination;
 }
