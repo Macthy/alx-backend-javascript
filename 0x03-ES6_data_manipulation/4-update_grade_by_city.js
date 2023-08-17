8807dd66117bb9ea86a5aae3c0a9de401fb11c59
thymac <thywillberdone@gmail.com>
2023-08-17 14:41:18 +0000

    revised

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,3 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  // Use filter function to get students for the specified city
-  const studentsInCity = students.filter((student) => student.location === city);
-
-  // Use map function to update grades for the students in the specified city
-  return studentsInCity.map((student) => {
-    const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
-    student.grade = gradeObj ? gradeObj.grade : 'N/A';
-    return student;
-  });
+  return students.filter((std) => std.location === city).map((std) => ({ ...std, grade: newGrades.filter((g) => g.studentId === std.id).map((g) => g.grade)[0] || 'N/A' }));
 }
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,14 @@
-/**
- * Function to return a string of set values that start with a specific string.
- * @param {Set} set - The set to check for values.
- * @param {string} startString - The string to check for at the beginning of the values.
- * @returns {string} A string of all the set values that start with the specified string.
- */
 export default function cleanSet(set, startString) {
-  // If no startString is provided, return an empty string.
-  if (!startString) {
-    return '';
-  }
+  let text = '';
+  const array = [];
 
-  // Filter the set values that start with the specified string and include only the rest of the string.
-  const filteredValues = [...set].filter((value) => value.startsWith(startString)).map((value) => value.substring(startString.length));
-  return filteredValues.join('-');
+  if (startString && typeof startString === 'string') {
+    for (const element of set) {
+      if (element && element.startsWith(startString)) {
+        array.push(element.slice(startString.length));
+      }
+    }
+    text = array.join('-');
+  }
+  return text;
 }
